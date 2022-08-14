a4f3e5f43fa9039bf3a3ef2edb8d535d7b2f1ece
IdrakHajizada <94737790+IdrakHajizada@users.noreply.github.com>
2022-08-15 00:21:43 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//Task No 4
+ function Start(){
+   let TestNum = prompt('Exam giymetivizi daxil edin');
+
+    if(TestNum > 80){
+        if(TestNum < 101){
+            alert('A')
+        }
+    }
+    else if(TestNum > 69){
+        alert('B')
+    }
+    else if(TestNum > 59){
+        alert('C')
+    }
+    else if(TestNum > 49){
+        alert('D')
+    }
+    else if(TestNum > -1){
+        alert('F')
+    }
+    else{
+        alert('Bele giymet olar ay eziz telebe')
+    }
+ }
+
+
+
+
